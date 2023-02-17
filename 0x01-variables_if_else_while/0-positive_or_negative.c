d509059f1cc118ea6ce6bcac371e1ef4c7abbbd9
Tim-T99 <timthiongo@gmail.com>
2023-02-17 10:22:55 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,6 @@
 #include <time.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -25,4 +24,4 @@ if (n == 0)
 printf("%d is zero\n", n);
 }
 return (0);
-}	
+}
